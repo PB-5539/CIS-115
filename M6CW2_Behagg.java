1bd60d09c8ae0168e21af1345c4e46608cede6f1
PB-5539 <behaggp5539@student.faytechcc.edu>
2026-04-19 19:40:27 -0400

    M6CW2

--- M6CW2_Behagg.java
+++ M6CW2_Behagg.java
@@ -1,3 +1,30 @@
+/**
+ * Parker Behagg
+ * 4/19/2026
+ * M6CW2
+ * area calculator
+ */
+import java.util.Scanner;
 public class M6CW2_Behagg {
-    
+    public static void main(String[] args) {
+        displayGreeting();
+        calculateArea();
+        displayClosing();
+    }
+    public static void displayGreeting() {
+        System.out.println("welcome!");
+    }
+    public static void calculateArea() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the length of the rectangle:");
+        double length = input.nextDouble();
+        System.out.print("enter the width of the rectangle:");
+        double width = input.nextDouble();
+        double area = length * width;
+        System.out.println("the area of the rectangle is: " + area);
+        input.close();
+    }
+    public static void displayClosing() {
+        System.out.println("thank you for using the area calculator!");
+    }
 }
